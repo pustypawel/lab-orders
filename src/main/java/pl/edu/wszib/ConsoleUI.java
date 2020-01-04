ecbe40434c9057947f1614aeecc60edf58a7e54c
ppusteln <pustypawel@gmail.com>
2020-01-04 17:13:01 +0100

    refactoring

--- src/main/java/pl/edu/wszib/ConsoleUI.java
+++ src/main/java/pl/edu/wszib/ConsoleUI.java
@@ -5,35 +5,45 @@ import java.util.Scanner;
 public class ConsoleUI {
 
     public void start() {
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldContinue;
         do {
-            System.out.println("1. Utworz zamówienie");
-            System.out.println("2. Wypisz zamówienia");
-            System.out.println("3. Edytuj zamówienie");
-            System.out.println("4. Usuń zamówienie");
-            System.out.println("0. Wyjście");
-            Scanner scanner = new Scanner(System.in);
+            showMenu();
             String line = scanner.nextLine();
             UserCommand command = UserCommand.parseUserCommand(line);
-            switch (command) {
-                case CREATE:
-                    createOrder();
-                    break;
-                case EDIT:
-                    editOrder();
-                    break;
-                case DELETE:
-                    deleteOrder();
-                    break;
-                case PRINT_ALL:
-                    printAll();
-                    break;
-                case EXIT:
-                    return;
-                case UNKNOWN:
-                    unknown();
-                    break;
-            }
-        } while (true);
+            shouldContinue = handleCommand(command);
+        } while (shouldContinue);
+    }
+
+    private boolean handleCommand(UserCommand command) {
+        switch (command) {
+            case CREATE:
+                createOrder();
+                break;
+            case EDIT:
+                editOrder();
+                break;
+            case DELETE:
+                deleteOrder();
+                break;
+            case PRINT_ALL:
+                printAll();
+                break;
+            case EXIT:
+                return false;
+            case UNKNOWN:
+                unknown();
+                break;
+        }
+        return true;
+    }
+
+    private void showMenu() {
+        System.out.println("1. Utworz zamówienie");
+        System.out.println("2. Wypisz zamówienia");
+        System.out.println("3. Edytuj zamówienie");
+        System.out.println("4. Usuń zamówienie");
+        System.out.println("0. Wyjście");
     }
 
     private void unknown() {

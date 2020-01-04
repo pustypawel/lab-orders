0ac99ea32ceafb3ac6734b08cb69ae854efd15c2
ppusteln <pustypawel@gmail.com>
2020-01-04 16:50:36 +0100

    handling commands

--- src/main/java/pl/edu/wszib/ConsoleUI.java
+++ src/main/java/pl/edu/wszib/ConsoleUI.java
@@ -18,5 +18,45 @@ public class ConsoleUI {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         UserCommand command = UserCommand.parseUserCommand(line);
+        switch (command) {
+            case CREATE:
+                createOrder();
+                break;
+            case EDIT:
+                editOrder();
+                break;
+            case DELETE:
+                deleteOrder();
+                break;
+            case PRINT_ALL:
+                printAll();
+                break;
+            case EXIT:
+                return;
+            case UNKNOWN:
+                unknown();
+                break;
+        }
     }
+
+    private void unknown() {
+        System.out.println("Unknown");
+    }
+
+    private void printAll() {
+        System.out.println("printAll");
+    }
+
+    private void deleteOrder() {
+        System.out.println("deleteOrder");
+    }
+
+    private void createOrder() {
+        System.out.println("createOrder");
+    }
+
+    private void editOrder() {
+        System.out.println("editOrder");
+    }
+
 }
--- src/main/java/pl/edu/wszib/Main.java
+++ src/main/java/pl/edu/wszib/Main.java
@@ -1,11 +1,8 @@
 package pl.edu.wszib;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.asList(UserCommand.values()));
-//        ConsoleUI consoleUI = new ConsoleUI();
-//        consoleUI.start();
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.start();
     }
 }
